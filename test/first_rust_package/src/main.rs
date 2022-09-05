e3483b2b3dbbdc491a1a80d24194d439fd5023dc
xgqfrms <xgqfrms@outlook.com>
2022-09-05 18:14:37 +0800

    feat: println {} 差值，占位符 & fn 定义 function

--- test/first_rust_package/src/main.rs
+++ test/first_rust_package/src/main.rs
@@ -2,4 +2,14 @@ fn main() {
     println!("🦀 Hello, world!");
     println!("\n$ rustc -V");
     println!("rustc 1.63.0 (4b91a6ea7 2022-08-08)");
+    add(1, 2);
+}
+
+// fn 定义 function
+fn add(left: usize, right: usize) -> usize {
+  // {} 差值，占位符
+  println!("\n🦀 left = {}, right = {}", left, right);
+  println!("🦀🦀 left + right = {}", left + right);
+  println!("\n🦀🦀🦀 left = {}, right = {}, left + right = {}", left, right, left + right);
+  left + right
 }
