40c079c72dd65dee13cb9812f870e2a40d738885
xgqfrms <xgqfrms@outlook.com>
2022-09-06 22:43:39 +0800

    feat: rm -rf dist && mkdir dist

--- src/hello_world/src/main.rs
+++ src/hello_world/src/main.rs
@@ -1,5 +1,9 @@
 // $ rustc main.rs -o main && ./main
 
+// $ rm -rf dist && mkdir dist
+// $ rustc src/main.rs -o ./dist/main && ./dist/main
+
+// $ cargo run
 // $ cargo run hello_world
 // cargo run 入口文件 `src/lib.rs` 或 `src/lib.rs`
 
