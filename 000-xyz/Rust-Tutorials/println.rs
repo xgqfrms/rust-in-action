59686f4dbe38ef34b2b3e56632d78cf64fd01c35
xgqfrms <xgqfrms@outlook.com>
2022-09-16 00:58:46 +0800

    feat: ✅ 转义符 = {\n}

--- 000-xyz/Rust-Tutorials/println.rs
+++ 000-xyz/Rust-Tutorials/println.rs
@@ -4,6 +4,16 @@ fn main () {
   println!("println! === print! + \\n 🦀");
   let year = 2022;
   println!("this year is {}", year);
+  // 转义符 {}
+  println!("转义符 = {{}}");
+  println!("转义符 = {{\\n}}");
+  // ✅ 转义符 = {\n}
+  // println!("转义符 = {\n}");
+  // ❌ error: aborting due to previous error
+  // println!("转义符 = {\n");
+  // ❌ ^ expected `'}'` in format string
+  // println!("转义符 = \{ \}");
+  // ❌ \ unknown character escape
 }
 
 
