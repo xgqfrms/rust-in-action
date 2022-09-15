42ea8a3f49df9b65b9c9e7f59f90ca79b6affd4b
xgqfrms <xgqfrms@outlook.com>
2022-09-16 01:22:49 +0800

    feat: let

---
+++ 000-xyz/Rust-Tutorials/const.rs
@@ -0,0 +1,27 @@
+
+/**
+ *
+ * @author xgqfrms
+ * @license MIT
+ * @copyright xgqfrms
+ * @created 2022-09-16
+ * @modified
+ *
+ * @description
+ * @link
+ *
+ */
+
+fn main () {
+  // print
+  print!("print 🦀");
+  print!("\n");
+  // println! === print! + "\n"
+  println!("println 🦀");
+}
+
+/*
+
+$ rustc ./const.rs && ./const
+
+*/
